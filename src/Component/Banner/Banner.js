0e113b6710b76f7d4273ff840ef41085836c5188
Amortha <amitsharma1998k@gmail.com>
2022-04-18 19:31:39 +0600

    commit

--- src/Component/Banner/Banner.js
+++ src/Component/Banner/Banner.js
@@ -10,7 +10,7 @@ const Banner = () => {
                         <div className="title">
                             <h1 className="text-light">Enjoy the tour with <br /> ghurbo</h1>
                             <h6 className="text-light">We provide travelers with an authentic Jordanian experience that they <br />
-                                can remember for the rest of their lives</h6>
+                                can remember for the rest of the lives</h6>
                         </div>
                     </div>
                 </div>
